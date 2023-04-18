0f0711874ddc693288bc0c3ed824e22ae2ba4ab3
Nadia Yasmin <116488494+nadia-yasmin@users.noreply.github.com>
2023-04-18 12:39:55 +0600

    Add files via upload

---
+++ test/java/com/example/khusbu/KhusbuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.khusbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhusbuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
